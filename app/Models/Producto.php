61d8bbec6a67a00e638349c97e59270a5e0df618
e.dinatale <69816085+1w3111909DiNataleEmiliano@users.noreply.github.com>
2023-01-26 02:10:53 -0300

    Primer Commit

---
+++ app/Models/Producto.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * Class Producto
+ *
+ * @property $id
+ * @property $descripción
+ * @property $precio
+ * @property $stock
+ * @property $created_at
+ * @property $updated_at
+ *
+ * @package App
+ * @mixin \Illuminate\Database\Eloquent\Builder
+ */
+class Producto extends Model
+{
+    
+    static $rules = [
+		'descripción' => 'required',
+		'precio' => 'required',
+		'stock' => 'required',
+    ];
+
+    protected $perPage = 20;
+
+    /**
+     * Attributes that should be mass-assignable.
+     *
+     * @var array
+     */
+    protected $fillable = ['descripción','precio','stock'];
+
+
+
+}
